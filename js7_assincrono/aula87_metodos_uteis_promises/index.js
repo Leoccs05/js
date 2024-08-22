4ca5d91e6e1dabc85b5ed78f78529e1ee3312ad5
Leoccs05 <leo_ccs05@hotmail.com>
2024-08-22 13:36:12 -0300

    correção de erro

--- js7_assincrono/aula87_metodos_uteis_promises/index.js
+++ js7_assincrono/aula87_metodos_uteis_promises/index.js
@@ -22,21 +22,21 @@ function esperaAi(msg, tempo){
 }
 
 //Promise.all - Se voce so quer que resolva e traga todos os valores, ou se falhar retorna so a falha
-/*const promises = [
+    const promises = [
     'primeiro valor',
     esperaAi('Promise 1', 3000),
     esperaAi('Promise 2', 500),
-    esperaAi(//*222* ou *'Promise 3'*, 1000),
+    esperaAi('222 para erro ou Promise 3 para executar certo', 1000),
     'Outro valor'
-]
+    ]
 
-Promise.all(promises)
+    Promise.all(promises)
     .then(function(valor){
         console.log(valor);
     })
     .catch(function(erro){
         console.log(erro);
-    })*/
+    })
 
     //Promise.race - retorna o 1° valor resolvido
     const promise = [
@@ -45,7 +45,7 @@ Promise.all(promises)
         esperaAi('Promise 3', rand(1, 5)),
         esperaAi(1000, rand(1, 5)),
        // 'Outro valor' entraga string normal primeiro
-    ]
+     ]
 
     Promise.race(promise)
     .then(function(valor){
