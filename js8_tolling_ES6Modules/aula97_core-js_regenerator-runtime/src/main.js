599d8490fa26971ff73111dfcd202a76cc761c99
Leoccs05 <leo_ccs05@hotmail.com>
2024-08-26 16:40:09 -0300

    Core-js e regenerator-runtime

---
+++ js8_tolling_ES6Modules/aula97_core-js_regenerator-runtime/src/main.js
@@ -0,0 +1,7 @@
+//Para rodar algumas em navegadores antigo
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+import executa from './modules/promise';
+
+import './assets/css/style.css';
+executa()
