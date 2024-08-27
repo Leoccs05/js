dd25f2369e451039e002d73a0ca02912f85fe7ae
Leoccs05 <leo_ccs05@hotmail.com>
2024-08-27 09:36:18 -0300

    modulo2

---
+++ node/aula02/app.js
@@ -0,0 +1,14 @@
+/*const multiplicacao = require('./mod')
+console.log(multiplicacao(2,2))*/
+/*const Cachorro = require('./B/C/D/mod')
+const dogMal =  new Cachorro('Princesa')
+dogMal.latir()
+
+console.log(__filename);//nome do arquivo atual
+console.log(__dirname); //nome da pasta atual
+const path = require('path')
+console.log(path.resolve(__dirname, '..', '..','arquivos', 'imagens'))*/
+
+const Cachorro = require('./Z/mod2.js')
+const c1 =  new Cachorro('Princesa')
+c1.latir()
