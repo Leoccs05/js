0dc34b732f0eb6e74dcf9d0f39008a3daa4df6f1
Leoccs05 <leo_ccs05@hotmail.com>
2024-08-28 11:45:25 -0300

    Express - Introdução

---
+++ node/aula006_express_introducao/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express();
+//        Cria    ler  Atualiza Apagar
+//CRUD -> Create, Read, Update, Delete
+//        POST    GET     PUT   DELETE
+// http://meusite.com/ <- GET -> Entreag a pagina "/"
+// http://meusite.com/sobre <- GET -> Entrega a pagina "/sobre"
+// http://meusite.com/contato <- GET -> Entrega a pagina "/contato"
+
+app.get('/', (requisicao, resposta) =>{
+    resposta.send(`
+        <form action="/" method="POST">
+            Nome: <input type="text" name="nome"></input>
+            <button>Enviar</button>
+        </form>
+    `)
+});
+
+app.post('/', (requisicao, resposta)=>{
+    resposta.send('Recebi o formulario')
+});
+
+app.get('/contato', (requisicao, resposta) =>{
+    resposta.send('Obrigado por entra em contato com a gente')
+});
+
+app.listen(3000, () => {
+    console.log('Servidor executando na porta 3000')
+    console.log('Acessar pagina principal http://localhost:3000');
+    console.log('Acessar pagina de contato http://localhost:3000/contato');
+});
+
+//como executar, pelo terminal do vs usando 'node server.js' e ctrl+c para parar de executar
+//pega a pasta e usar cd do cmd 
