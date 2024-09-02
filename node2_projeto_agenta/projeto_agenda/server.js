0e46b9ec10651d2ff3b74f6486a2c9bf07908ed5
Leoccs05 <leo_ccs05@hotmail.com>
2024-09-02 19:28:10 -0300

    Logando o usuário

--- node2_projeto_agenta/projeto_agenda/server.js
+++ node2_projeto_agenta/projeto_agenda/server.js
@@ -65,6 +65,6 @@ app.on('pronto', () => {
     app.listen(3000, () => {
         console.log('Servidor executando na porta 3000')
         console.log('Acessar pagina principal http://localhost:3000');
-        console.log('Acessar pagina de contato http://localhost:3000/contato');
+        console.log('Acessar pagina de contato http://localhost:3000/login/index');
     });
 })
--- node2_projeto_agenta/projeto_agenda/src/models/loginModel.js
+++ node2_projeto_agenta/projeto_agenda/src/models/loginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcryptjs = require('bcryptjs')
 
 //trata os dados para o mongoDB por isso usa o mongoose
 const LoginSchema = new mongoose.Schema({
@@ -16,17 +17,44 @@ class Login{
         this.user = null;
     }
 
+    async login(){
+        this.valida();
+        if(this.errors.length > 0) return;
+        this.user = await LoginModel.findOne({ email: this.body.email})
+
+        if(!this.user){
+            this.errors.push('Usuario não existe')
+            return;
+        } 
+
+        if (!bcryptjs.compareSync(this.body.password, this.user.password)){
+            this.errors.push('Senha invalida')
+            this.user = null;
+            return;
+        }
+    }
+
     async register(){
         this.valida()
         if(this.errors.length > 0) return;
-        try{
-            this.user = await LoginModel.create(this.body)
-        }catch(e){
-            console.log(e)
-        }
-        
+
+       await this.userExists();
+
+       if(this.errors.length > 0) return;
+
+       //'cryptografa' senha
+       const salt = bcryptjs.genSaltSync()
+       this.body.password = bcryptjs.hashSync(this.body.password, salt)
+       
+        this.user = await LoginModel.create(this.body)
     }
 
+    async userExists(){
+     this.user = await LoginModel.findOne({ email: this.body.email})
+      if(this.user) this.errors.push('Usuario já existe')
+    }
+
+    
     valida(){
         this.cleanUp()
 
