7b2811e39849463419d5bec6ef01abcf33938008
Leoccs05 <leo_ccs05@hotmail.com>
2024-09-06 00:00:11 -0300

    dada

---
+++ node3_apireset_mysql/mysql/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mysql = require('mysql2')
+const port = 3000;
+const app = express()
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'curso_node',
+  password: '',
+});
+
+// chamando a função que conecta com o banco de dados
+conn.connect((err) => {
+  if (err) {
+    console.log('Erro ao tentar se conectar com o banco de dados');
+  }
+
+  console.log('Conectado com o banco de dados');
+
+  // rodando a aplicação somente quando conseguir se conectar com o banco de dados
+  app.listen(port, () => {
+    console.log('Servidor rodando corretamente');
+  });
+});
+
