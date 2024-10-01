2fab383eeb984bb36e26da951b5d29a9ede9b653
Leoccs05 <leo_ccs05@hotmail.com>
2024-10-01 17:24:14 -0300

    Páginas e rotas da aplicação

---
+++ react_iniciante/consome-api/src/pages/Page404/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from '../../styles/GlobalStyles'
+//import history from '../../services/history';
+
+export default function Page404(){
+  //history.push('/')
+  return(
+    <Container>
+      <h1>Essa Pagina não Existe</h1>
+    </Container>
+  )
+}
