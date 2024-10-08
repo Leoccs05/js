a558f51b05bdd832d25ee871c096d772c91f802a
Leoccs05 <leo_ccs05@hotmail.com>
2024-10-08 17:39:23 -0300

    Exibindo a foto do aluno

--- react_iniciante/consome-api/src/pages/Aluno/index.js
+++ react_iniciante/consome-api/src/pages/Aluno/index.js
@@ -4,23 +4,26 @@ import { get } from 'lodash';
 import { toast } from "react-toastify";
 import {isEmail, isInt, isFloat} from 'validator'
 import { useDispatch } from "react-redux";
+import {FaUserCircle, FaEdit} from 'react-icons/fa'
+import { Link } from 'react-router-dom';
 
 import axios from '../../services/axios'
 import history from '../../services/axios'
 import PropTypes from "prop-types";
-import { Form } from './styled'
+import { Form, ProfilePicture } from './styled'
 import Loading from '../../components/Loading'
 import * as actions from '../../store/modules/auth/actions'
 
 export default function Aluno({ match }) {
   const dispatch = useDispatch()
 
-  const id = get(match, 'params.id', 0);
+  const id = get(match, 'params.id', '');
   const [nome, setNome] = useState('');
   const [sobrenome, setSobrenome] = useState('');
   const [email, setEmail] = useState('');
   const [idade, setIdade] = useState('');
   const [peso, setPeso] = useState('');
+  const [foto, setFoto] = useState('');
   const [altura, setAltura] = useState('');
   const [isLoading, setisLoading] = useState(false);
 
@@ -32,6 +35,7 @@ export default function Aluno({ match }) {
         const {data} = await axios.get(`/alunos/${id}`)
         const Foto = get(data, 'Fotos[0].url','')
 
+        setFoto(Foto)
         setNome(data.nome)
         setSobrenome(data.sobrenome)
         setEmail(data.email)
@@ -123,6 +127,20 @@ export default function Aluno({ match }) {
       <Loading isLoading={isLoading}/>
       <h1>{id ? 'Editar Aluno' : 'Registrar Novo Aluno'}</h1>
 
+      {id && (
+        <ProfilePicture>
+          {foto ?
+          ( <img src={foto} alt={nome}/>
+
+          ) : (
+              <FaUserCircle size={180}/>
+          )}
+          <Link to={`/fotos/${id}`}>
+            <FaEdit size={24}/>
+          </Link>
+        </ProfilePicture>
+      )}
+      
       <Form onSubmit={handleSubmit}>
       <input type="text" value={nome} onChange={e=> setNome(e.target.value)} placeholder='Nome'/>
       <input type="text" value={sobrenome} onChange={e=> setSobrenome(e.target.value)} placeholder='Sobrenome'/>
