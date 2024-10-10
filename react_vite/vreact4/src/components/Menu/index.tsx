8c165ac19ee431a25147294f1ddcf7c543cd9b33
Leoccs05 <leo_ccs05@hotmail.com>
2024-10-10 16:01:10 -0300

    state e useLocation

--- react_vite/vreact4/src/components/Menu/index.tsx
+++ react_vite/vreact4/src/components/Menu/index.tsx
@@ -8,7 +8,7 @@ export const Menu = () =>{
                     <Link to="/">Home</Link>
                 </li>
                 <li>
-                    <Link to="/about">About</Link>
+                    <Link to="/about" state={'This is state from About'}>About</Link>
                 </li>
                 <li>
                     <Link to="/posts">Posts</Link>
