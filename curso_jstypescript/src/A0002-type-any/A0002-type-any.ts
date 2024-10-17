74be277f66d2c3ccb1c94fa3446c66da75fe90f6
Leoccs05 <leo_ccs05@hotmail.com>
2024-10-17 14:08:59 -0300

    type any

---
+++ curso_jstypescript/src/A0002-type-any/A0002-type-any.ts
@@ -0,0 +1,7 @@
+//utilizar 'any' apenas em ultimo caso
+function showMessage(msg: string){
+  return msg;
+}
+//console.log(showMessage([1, 2, 3]))
+console.log(showMessage('Olá'));
+//console.log(showMessage(1))
